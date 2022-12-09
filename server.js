9d2e695e45c03a93fbe6469812beb65bc8ceff87
haithem711 <49453214+haithem711@users.noreply.github.com>
2022-12-09 17:53:41 +0100

    fix files

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const contactUs = require("./routes/contact");
 const auth = require("./routes/auth");
 const doctor = require("./routes/doctor");
 const config = require("dotenv").config();
-const path = require("path");
 const app = express();
 const PORT = 5000;
 /* -------------------------------------------------------------------------- */
@@ -38,17 +37,23 @@ app.use("/api", doctor);
 /* -------------------------------------------------------------------------- */
 /*                               CONNECT SERVER                               */
 /* -------------------------------------------------------------------------- */
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  );
-});
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       if (err) {
+//         res.status(500).send(err);
+//       }
+//     }
+//   );
+// });
 
-
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
