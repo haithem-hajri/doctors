6f527126345fad724722bdcd4489f315bf898756
haithem711 <49453214+haithem711@users.noreply.github.com>
2022-12-17 13:18:48 +0100

    vercel

--- routes/contact.js
+++ routes/contact.js
@@ -4,3 +4,9 @@ const { addContact, getContacts } = require("../controllers/contact");
 router.post("/contact-us", addContact);
 router.get("/contact-us", getContacts);
 module.exports = router;
+
+// "server": "node server.js",
+// "client": "npm start --prefix client",
+// "clientinstall": "npm install --prefix client",
+// "dev": "concurrently \"npm run server\" \"npm run client\"",
+// "render-postbuild": "NPM_CONFIG_PRODUCTION=false npm install && npm install --prefix client && npm run build --prefix client"
